23b79b051f7306cec71ab56c5d8b1839db8f778b
nikolettab <nikoletta.buza@liferay.com>
2025-05-04 22:58:17 +0200

    Test-project-00 GUI elements

--- src/FilteredResult.java
+++ src/FilteredResult.java
@@ -1,15 +1,89 @@
 import javax.swing.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.beans.XMLEncoder;
 import java.io.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FilteredResult extends JFrame {
+
+    private JPanel jPanel;
+    private JCheckBox selectYearCheckBox;
+    private JList list1;
+    private JCheckBox selectWeekCheckBox;
+    private JList list2;
+    private JCheckBox selectDateCheckBox;
+
+    public FilteredResult() {
+        setTitle("Filtered Lottery Results");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        selectYearCheckBox.setSelected(true);
+        selectWeekCheckBox.setEnabled(true);
+        selectWeekCheckBox.setEnabled(false);
+        selectDateCheckBox.setEnabled(true);
+
+        selectYearCheckBox.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                // When the year checkbox is selected,
+                //      the week checkbox should be enabled but not yet selected;
+                //      the date checkbox should be enabled but not selected;
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    selectWeekCheckBox.setEnabled(true);
+                    selectWeekCheckBox.setSelected(false);
+                    selectDateCheckBox.setEnabled(true);
+                    selectDateCheckBox.setSelected(false);
+                }
+                // When the year checkbox is deselected,
+                //      the week checkbox should be deselected and disabled
+                //      the date checkbox should be enabled and deselected
+                else if (e.getStateChange() == ItemEvent.DESELECTED) {
+                    selectWeekCheckBox.setEnabled(false);
+                    selectWeekCheckBox.setSelected(false);
+                    selectDateCheckBox.setEnabled(true);
+                    selectDateCheckBox.setSelected(false);
+                }
+            }
+        });
+
+        selectDateCheckBox.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                // When the date checkbox is selected
+                //      the year checkbox should be disabled and deselected
+                if (e.getStateChange() == ItemEvent.SELECTED) {
+                    selectYearCheckBox.setEnabled(false);
+                    selectYearCheckBox.setSelected(false);
+                    selectWeekCheckBox.setEnabled(false);
+                    selectWeekCheckBox.setSelected(false);
+                }
+                // When the date checkbox is deselected
+                //      the year checkbox should be enabled and deselected
+                else if (e.getStateChange() == ItemEvent.DESELECTED) {
+                    selectYearCheckBox.setEnabled(true);
+                    selectYearCheckBox.setSelected(false);
+                    selectWeekCheckBox.setEnabled(false);
+                    selectWeekCheckBox.setSelected(false);
+                }
+            }
+        });
+    }
 
     public static void main(String[] args) {
 
         // Create a list of objects that contain values by which we can filter
         ArrayList<FilterValues> filterValuesList = new ArrayList<>();
         addFilterListElementsToUI(filterValuesList);
+
+        // Render UI
+        // UI is disabled
+//        new FilteredResult();
 
         // Get filter value(s) from the UI after hitting the "Search" button
         // TODO ui.getFilterBy ==> year / year & week / date
